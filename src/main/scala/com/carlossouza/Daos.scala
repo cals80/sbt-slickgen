718412a81c93e78fd41330c1b4f8320ebbe1947a
cals80 <souza.capple@gmail.com>
2016-04-24 02:01:35 -0300

    Implementing gen-daos

--- src/main/scala/com/carlossouza/Daos.scala
+++ src/main/scala/com/carlossouza/Daos.scala
@@ -1,5 +1,7 @@
 package com.carlossouza
 
+import java.io.PrintWriter
+
 import scala.collection.mutable
 import scala.io.Source
 
@@ -9,7 +11,7 @@ import scala.io.Source
 object Daos {
 
   def generate: Unit = {
-    getTables.foreach(println)
+    getTables.foreach(createDaoFile)
   }
 
   def getTables: List[String] = {
@@ -17,11 +19,40 @@ object Daos {
     val caseClassFilePath = Settings.outputDir + Settings.packageDir + "/TableRows.scala"
     for (line <- Source.fromFile(caseClassFilePath).getLines()) {
       if (line.startsWith("case class")) {
-        val className: String = line.split('(')(0).replace("case class ", "")
+        val className: String = line.split('(')(0).replace("case class ", "").replace("Row", "")
         result += className
       }
     }
     result.toList
+  }
+
+  def createDaoFile(className: String): Unit = {
+    val packageName = if (Settings.playFramework) "dao" else Settings.packageName
+    val modelsName = if (Settings.playFramework) "models" else Settings.packageName
+    val targetFile = if (Settings.playFramework) Settings.outputDir + "dao/" + className + "DAO.scala" else Settings.outputDir + className + "DAO.scala"
+    val preset: Seq[String] = Seq(
+      "package " + packageName + "\n\n",
+      "import " + modelsName + ".{" + className + "Row, Tables}\n",
+      "import javax.inject.Inject\n",
+      "import scala.concurrent._\n",
+      "import slick.driver.MySQLDriver\n",
+      "import slick.driver.MySQLDriver.api.\n\n",
+      "class " + className + "DAO @Inject() (db: MySQLDriver.backend.Database) {\n",
+      "\t" + "def fetchAll(): Future[Seq[" + className + "Row]] = db.run(Tables." + className + ".result)\n",
+      "\t" + "def count(): Future[Int] = db.run(Tables." + className + ".length.result)\n",
+      "\t" + "def insert(newRow: " + className + "Row): Future[Int] = db.run((Tables." + className + " returning Tables." + className + ".map(_.id)) += newRow)\n",
+      "\t" + "def save(maybeNewRow: " + className + "Row): Future[Int] = db.run(Tables." + className + ".insertOrUpdate(maybeNewRow))\n",
+      "\t" + "def findById(id: Int): Future[Option[" + className + "Row]] = db.run(Tables." + className + ".filter(_.id === id).result.headOption)\n",
+      "\t" + "def deleteById(id: Int): Future[Int] = db.run(Tables." + className + ".filter(_.id === id).delete)\n",
+      "\t" + "def update(updatedRow: " + className + "Row): Future[Int] = db.run(Tables." + className + ".filter(_.id === updatedRow.id).update(updatedRow))\n",
+      "}"
+    )
+    val fileOut = new PrintWriter(targetFile, "UTF-8")
+    try {
+      preset.foreach(presetLine => fileOut.print(presetLine))
+    } finally {
+      fileOut.close()
+    }
   }
 
 }
